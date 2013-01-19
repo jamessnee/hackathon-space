55952385f3563a0b271b6d7c26cfd758022ea090
O.R.A. Chick <oc243@pccl055.pwf.cl.cam.ac.uk>
2013-01-19 20:43:01 +0000

    This may break the build. Added kinect stuff to get pos from camera

--- Pi_Client/pi_client.py
+++ Pi_Client/pi_client.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 
 import pygame
+import freenect
+import frame_convert
 from pygame.locals import *
 from twisted.internet import reactor
 from twisted.internet.task import LoopingCall
@@ -49,8 +51,30 @@ class kplayer(DatagramProtocol):
 	def connectionRefused(self):
 		print "Refused"
 
+
+def get_depth():
+    return frame_convert.pretty_depth(freenect.sync_get_depth()[0])
+
+def to_len(ar):
+    for x in range(1, len(ar)-1):
+        ar[x] = ar[x-1] * ar[x] + ar[x]
+    return ar
+        
+def max_run(ar):
+    max = i = pos = 0
+    for x in ar:
+        i += 1
+        if x > max:
+            max = x
+            pos = i
+    return pos - max / 2
+
+
 def get_kplayer_pos_from_kinect():
-	return 100
+    x = get_depth()[200]
+    y = map (lambda x : 1 if x < 200 else 0, x)
+    return max_run(to_len(y)) / 10
+
 
 def pygame_loop():
 	"""Draw the background"""
@@ -68,7 +92,7 @@ def pygame_loop():
 
 
 	pygame.display.update()
-	player.move(10)
+	player.move(get_kplayer_pos_from_kinect())
 
 
 if __name__ == "__main__" :
