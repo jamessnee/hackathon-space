1d3dbc0ae19556f419ee99bc1a6b313a9f224dc9
James Snee <james.snee@gmail.com>
2013-01-19 15:17:24 +0000

    messed up

--- Pi_Client/pi_client.py
+++ Pi_Client/pi_client.py
@@ -1,21 +1,41 @@
-#! /usr/bin/env python
 import pygame
+from pygame.locals import *
 
-IMAGE_DIR = "Images/"
-IMAGE_SPACESHIP = IMAGE_DIR+"hajo1.png"
+class App:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self._image_surf = None
+	
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode((350,350), pygame.HWSURFACE)
+        self._running = True
+        self._image_surf = pygame.image.load("myimage.jpg").convert()
+	
+    def on_event(self, event):
+        if event.type == QUIT:
+            self._running = False
+    def on_loop(self):
+        pass
+    def on_render(self):
+        self._display_surf.blit(self._image_surf,(0,0))
+        pygame.display.flip()
+	
+    def on_cleanup(self):
+        pygame.quit()
+	
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+		
+        while( self._running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
 
-
-
-def parse_server_message(message):
-	print "Parsing the server's message"
-
-def draw_screen(player,dropped_items):
-	print "Drawing the player"
-	print "Drawing the dropped items"
-
-if __name__=="__main__":
-	print "Do that funky thing!"
-	screen = pygame.display.set_mode((640,480))
-	spaceship = pygame.image.load(IMAGE_SPACESHIP)
-	background = pygame.image.load("Images/background.png")
-	screen.blit(background,(0,0))
+if __name__ == "__main__" :
+    theApp = App()
+    theApp.on_execute()
