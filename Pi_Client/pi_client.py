2d0d0e140a88090248b91e74d1bc7f7bb9a56f9c
James Snee <jas250@cl.cam.ac.uk>
2013-01-19 13:30:17 +0000

    Added basic method stubs for parsing the server's message and drawing the screen

--- Pi_Client/pi_client.py
+++ Pi_Client/pi_client.py
@@ -1,4 +1,14 @@
 #! /usr/bin/env python
 
-if __name__="__main__":
-	
+def parse_server_message(message):
+	print "Parsing the server's message"
+
+def draw_screen(player,dropped_items):
+	print "Drawing the player"
+	print "Drawing the dropped items"
+
+if __name__=="__main__":
+	print "Do that funky thing!"
+	while(1):
+		parse_server_message('')
+		draw_screen('','')
