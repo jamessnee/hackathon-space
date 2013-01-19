797445ac76907b2c0ae2a51bc31c3ea1f574a117
Andrew Cooper <andrew.cooper3@citrix.com>
2013-01-19 20:07:55 +0000

    Basic kplayer connectivity

--- Game_Server/main.py
+++ Game_Server/main.py
@@ -5,18 +5,69 @@ import sys
 from twisted.internet import reactor
 from twisted.internet.protocol import DatagramProtocol
 
-class Echo(DatagramProtocol):
+import game
 
-    def datagramReceived(self, data, (host, port)):
-        print "Got '%s' from %s:%d" % (data.strip(), host, port)
-        self.transport.write(data, (host, port))
+gl = None
+
+class UDPDemux(DatagramProtocol):
+
+    def __init__(self, nh):
+        self.nh = nh
+
+    def datagramReceived(self, data, addr):
+        print "UDP %s:%d -> %s" % (addr[0], addr[1], data.rstrip())
+        self.nh.received(self.transport, data, addr)
+
+class NetworkHandler(object):
+
+    def __init__(self):
+
+        self.players = {}
+
+    def listen(self):
+
+        self.udp = UDPDemux(self)
+        print "Listening on 9999 for UDP players"
+        reactor.listenUDP(9999, self.udp)
+
+    # Received data from the network
+    def received(self, trans, data, addr):
+
+        # If known, pass on to player
+        if addr in self.players:
+            self.players[addr].received(data)
+            return
+
+        # If new, try to create new player
+        elif data.startswith("REQ CON"):
+            ptype = data[7:].strip()
+
+            if ptype == "K":
+                p = game.Kplayer(self, trans, addr, gl)
+                gl.add_kplayer(p)
+                return
+            elif ptype == "D":
+                p = game.Dropper(self, trans, addr, gl)
+                gl.add_dropper(p)
+                return
+
+        # Or give up
+        trans.write("ERROR", addr)
+
+    def add(self, player):
+        assert player.addr not in self.players
+        self.players[player.addr] = player
+
+    def remove(self, player):
+        assert player.addr in self.players
+        del self.players[player.addr]
 
 
 if __name__ == "__main__":
-    print "Listening on port 9999"
-    reactor.listenUDP(9999, Echo())
 
-    print "Listening on port 9998"
-    reactor.listenUDP(9998, Echo())
+    nh = NetworkHandler()
+    gl = game.Logic()
+
+    nh.listen()
 
     reactor.run()
