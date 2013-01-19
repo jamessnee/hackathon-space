d585b8c55cf336ab33986f56a25ef2759f853304
James Snee <jas250@cl.cam.ac.uk>
2013-01-19 18:17:27 +0000

    Networking goodness

--- Pi_Client/pi_client.py
+++ Pi_Client/pi_client.py
@@ -1,11 +1,88 @@
+#! /usr/bin/env python
+
 import pygame
 from pygame.locals import *
+from twisted.internet import reactor
+from twisted.internet.task import LoopingCall
+from twisted.internet.protocol import DatagramProtocol
+
+
+class kplayer(DatagramProtocol):
+	def __init__(self):
+		self.x_pos = 0
+		self.lives = 1
+		self.dropping_objects = [(10,10)]
+		self.connection = False
+
+	def move(self,delta):
+		self.x_pos = delta
+		if False:
+			print "Sending players new position"
+			to_send = "KSEND %di,%d",int(self.x_pos[0]),int(self.x_pos[1])
+			self.transport.write(to_send)
+
+	def startProtocol(self):
+		host = "131.111.179.97"
+		port = 9999
+		
+		print "Opening connection"
+		self.transport.connect(host,port)
+		self.transport.write("REQ CON K")
+
+	def datagramReceived(self, data, (host,port)):
+		data_strip = data.strip()
+		if data_strip == "PING":
+			if self.connection:
+				print "Ping > PONG"
+				self.transport.write("PONG")
+		elif data_strip == "REQ OK":
+			print "Connection accepted"
+			self.connection = True
+		elif data_strip == "REQ REJECT":
+			print "Connection rejected"
+			self.connection = False
+		elif data_strip == "ERROR":
+			print "There was an error"
+		else:
+			print "Couldn't parse:",data.strip()
+
+	def connectionRefused(self):
+		print "Refused"
+
+def get_kplayer_pos_from_kinect():
+	return 100
+
+def pygame_loop():
+	"""Draw the background"""
+	window.blit(background,(0,0))
+
+	"""Draw the kplayer"""
+	window.blit(spaceship,(player.x_pos,410))
+
+	"""Draw a dropping object"""
+	for dropped in player.dropping_objects:
+		carrot = pygame.image.load("Images/carrot.png").convert()
+		window.blit(carrot,(dropped[0],dropped[1]))
+
+	"""Do some collision detection"""
+
+
+	pygame.display.update()
+	player.move(10)
+
 
 if __name__ == "__main__" :
-	screen = pygame.display.set_mode((640,480))
+	pygame.init()
+	window = pygame.display.set_mode((640, 480))
 	background = pygame.image.load("Images/background.png").convert()
-	screen.blit(background,(0,0))
-	while 1:
-		screen.blit(background,(0,0))
-		pygame.display.update()
-		pygame.time.delay(100)
+	spaceship = pygame.image.load("Images/hajo1.png").convert()
+
+	player = kplayer()
+
+	window.blit(background,(0,0))
+
+	reactor.listenUDP(0,player)	
+	lp = LoopingCall(pygame_loop)
+	lp.start(1)
+	reactor.run()
+
